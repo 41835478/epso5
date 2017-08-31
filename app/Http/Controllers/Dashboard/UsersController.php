2af5a9a871c5e738f59a7e73de002f2636640362
daguilarm <damian.aguilarm@gmail.com>
2017-08-31 22:37:46 +0200

    Simplify problem with roles

--- app/Http/Controllers/Dashboard/UsersController.php
+++ app/Http/Controllers/Dashboard/UsersController.php
@@ -27,12 +27,44 @@ class UsersController extends DashboardController
     {
         $this->controller = $controller;
         $this->table = $table;
-
         //Sharing in the view
         view()->share([
             'section' => 'users',
             'role' => 'user'
         ]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //Only editor can access this
+        if(!Credentials::isEditor()) {
+            return Credentials::accessError();
+        }
+        //
+        return $this->table
+            ->setValue([
+                'action' => 'users.actions'
+            ])
+            ->render(dashboard_path('users.index'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     * @return \Illuminate\Http\Response
+     */
+    public function create(ClientsRepository $client)
+    {
+        //Only editor can access this
+        if(!Credentials::isEditor()) {
+            return Credentials::accessError();
+        }
+        //
+        $clients = $client->lists(['id', 'client_name'], set_true('I want the firt option field empty!!!'));
+        return view(dashboard_path('users.create'), compact('clients'));
     }
 
     /**
@@ -43,16 +75,13 @@ class UsersController extends DashboardController
     public function edit(int $id, ClientsRepository $client)
     {
         $clients = $client->lists(['id', 'client_name'], set_true('I want the firt option field empty!!!'));
-
         $data = $this->controller
             ->find($id);
-
         //Check if the user own the database record
         //and if the role is authorizate
         if(!Credentials::authorize($data)) {
             return Credentials::accessError();
         }
-
         return view(dashboard_path('users.edit'), compact('clients', 'data'));
     }
 
@@ -66,15 +95,13 @@ class UsersController extends DashboardController
     {
         //Check if the user own the database record
         //and if the role is authorizate in: App\Repositories\Repository
-                
         $update = $this->controller
             ->store($id);
-
         return $update 
             ? redirect()
                 ->route(Credentials::isEditor() 
                     //God, Admin and Editor case
-                    ? 'dashboard.editor.users.index' 
+                    ? 'dashboard.user.users.index' 
                     //User case
                     : 'dashboard.user.users.edit', Credentials::id()
                 )
@@ -84,6 +111,33 @@ class UsersController extends DashboardController
                 ->withInput()
                 ->withErrors([
                 __('An error occurred during the updating process'), 
+                __('If the error persist, please contact with the system administrator')
+            ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param  \App\Http\Requests\UsersRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(UsersRequest $request)
+    {
+        //Only editor can access this
+        if(!Credentials::isEditor()) {
+            return Credentials::accessError();
+        }
+        //
+        $create = $this->controller
+            ->store();
+        return $create 
+            ? redirect()
+                ->route('dashboard.user.users.index')
+                ->withStatus(__('The item has been create successfuly'))
+            : redirect()
+                ->back()
+                ->withInput()
+                ->withErrors([
+                __('An error occurred during the creating process'), 
                 __('If the error persist, please contact with the system administrator')
             ]);
     }
--- routes/modules/userRoutes.php
+++ routes/modules/userRoutes.php
@@ -7,6 +7,8 @@ Route::group([
     ], function () {
         //Routes for the GOD tools
         Route::get('tools/role/{id}', 'Dashboard\God\ToolsController@role')->name('tools.role');
+
         //Users routes, the destroy, create or list options are in the editorRoutes.php
-        Route::resource('users', 'Dashboard\UsersController', ['only' => ['edit', 'update']]);  
+        Route::resource('users', 'Dashboard\UsersController', ['except' => ['destroy', 'show']]); 
+        Route::name('users.eliminate')->post('users/eliminate', 'Dashboard\UsersController@eliminate'); 
 });
