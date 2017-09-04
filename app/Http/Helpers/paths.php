60903a520a800bd5ed854bd54ade1e32774f4b95
daguilarm <damian.aguilarm@gmail.com>
2017-09-04 16:43:36 +0200

    Upgrade helpers

--- app/Http/Helpers/paths.php
+++ app/Http/Helpers/paths.php
@@ -6,9 +6,9 @@
  * @return string
  */
 if (!function_exists('action_path')) {
-    function action_path($file = null)
+    function action_path($file = 'default:action')
     {
-        return component_path('actions.') . (($file === null) ? 'edit' : $file);
+        return component_path('actions.') . $file;
     }
 }
 
@@ -69,7 +69,7 @@ if (!function_exists('form_path')) {
  * @return string
  */
 if (!function_exists('legend_path')) {
-    function legend_path($file = 'default')
+    function legend_path($file = 'default:legend')
     {
         return dashboard_path('_components.legends.') . $file;
     }
