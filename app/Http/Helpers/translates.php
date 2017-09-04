7de5e2526c500b70122b8abbacad28c518d97e00
daguilarm <damian.aguilarm@gmail.com>
2017-09-04 10:25:13 +0200

    Create new helper

--- app/Http/Helpers/translates.php
+++ app/Http/Helpers/translates.php
@@ -4,6 +4,7 @@
  * Return the singular or plural title for a section, in a language file
  * @param   $section
  * @param   $type ['singular', 'plural']
+ * 
  * @return  string
  */
 if (!function_exists('trans_title')) {
@@ -13,5 +14,18 @@ if (!function_exists('trans_title')) {
             return trans('sections/' . $section . '.title:plural');
         }
         return trans('sections/' . $section . '.title');
+    }
+}
+
+/**
+ * Return the slanguage file in the section folder
+ * @param   $file
+ * 
+ * @return  string
+ */
+if (!function_exists('sections')) {
+    function sections(string $file)
+    {
+        return trans('sections/' . $file);
     }
 }
