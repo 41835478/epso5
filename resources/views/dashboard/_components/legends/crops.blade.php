6db95cb6243f636bad3362e91b37db673748afa6
daguilarm <damian.aguilarm@gmail.com>
2017-09-05 15:19:01 +0200

    Updated

--- resources/views/dashboard/_components/legends/crops.blade.php
+++ resources/views/dashboard/_components/legends/crops.blade.php
@@ -1,6 +1,7 @@
 <ul>
     <li><span class="btn btn-warning">{!! icon('edit') !!}</span> {!! trans('legends.edit') !!}</li>
     <li><span class="btn btn-success">{!! icon('crops') !!}</span> {!! trans('legends.crop:variety') !!}</li>
+    <li><span class="btn btn-terciary">{!! icon('crop-alt') !!}</span> {!! trans('legends.crop:type') !!}</li>
     <li><span class="btn btn-danger">{!! icon('pests') !!}</span> {!! trans('legends.crop:pests') !!}</li>
     <li><span class="btn btn-info">{!! icon('tree') !!}</span> {!! trans('legends.crop:patterns') !!}</li>
 </ul>
---
+++ resources/views/dashboard/crop_varieties/forms/sections/default.blade.php
@@ -0,0 +1,27 @@
+<div class="row">
+
+    {{-- Row id --}}
+    {!! BootForm::hidden('row_id')->value($data->id ?? null) !!}
+
+    {{-- Variety: name --}}
+    {!! BootForm::text(trans('persona.name'), 'crop_variety_name')
+        ->addGroupClass('col-md-4')
+        ->autofocus()
+        ->required()
+    !!}
+
+    {{-- Crop: name --}}
+    {!! BootForm::select(trans_title('crops', 'singular'), 'crop_id')
+        ->addGroupClass('col-md-2')
+        ->options($crops ?? [])
+        ->defaultValue('es')
+        ->required()
+    !!}
+
+    {{-- Variety: type --}}
+    {!! BootForm::text(sections('crop_varieties.type'), 'crop_variety_type')
+        ->addGroupClass('col-md-4')
+        ->autofocus()
+        ->required()
+    !!}
+</div>
