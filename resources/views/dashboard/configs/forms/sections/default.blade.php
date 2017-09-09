54b052ddb3107426adb5956ab56c840cc6692b64
daguilarm <damian.aguilarm@gmail.com>
2017-09-09 10:49:49 +0200

    created the default form

--- resources/views/dashboard/configs/forms/sections/default.blade.php
+++ resources/views/dashboard/configs/forms/sections/default.blade.php
@@ -3,33 +3,15 @@
     {{-- Row id --}}
     {!! BootForm::hidden('row_id')->value($data->id ?? null) !!}
 
-    {{-- Input --}}
-    {!! BootForm::text(trans('persona.name'), 'name')
+    {{-- Configuration: name --}}
+    {!! BootForm::text(trans('persona.name'), 'config_name')
         ->addGroupClass('col-md-4')
         ->autofocus()
         ->required()
     !!}
 
-    {{-- Select --}}
-    {!! BootForm::select(trans('persona.locale'), 'locale')
-        ->addGroupClass('col-md-2')
-        ->options(select('locale') ?? [])
-        ->defaultValue('es')
-        ->required()
+    {{-- Configuration: description --}}
+    {!! BootForm::text(trans('base.description'), 'config_description')
+        ->addGroupClass('col-md-8')
     !!}
-
-    {{-- Addon --}}
-    {!! BootForm::InputGroup(trans('base.date'), 'agronomic_date')
-        ->addGroupClass('col-lg-2 col-md-6')
-        ->addClass('date')
-        ->afterAddon(icon('calendar'))
-        ->required() 
-    !!}
-
-    {{-- Field: Conditional role --}}
-    {{-- @Role('admin')
-        //
-    @else 
-        //
-    @endRoles --}}
 </div>
