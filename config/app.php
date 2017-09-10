5413e8f2616112e244ab22bc095103a4f249c694
daguilarm <damian.aguilarm@gmail.com>
2017-09-10 13:31:35 +0200

    Add new config variables

--- config/app.php
+++ config/app.php
@@ -12,7 +12,8 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Agroepso',
+    'name'      => 'Agroepso',
+    'version'   => '5.4.1',
 
     /*
     |--------------------------------------------------------------------------
