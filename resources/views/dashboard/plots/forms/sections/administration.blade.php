3428a2651924ba71a819e18462e2ca1ade2eac9d
daguilarm <damian.aguilarm@gmail.com>
2017-09-11 19:06:54 +0200

    Add client field

--- resources/views/dashboard/plots/forms/sections/administration.blade.php
+++ resources/views/dashboard/plots/forms/sections/administration.blade.php
@@ -7,12 +7,16 @@
             ->select(0)
             ->required()
         !!}
+
         {{-- Field: User --}}
         {!! BootForm::select(sections('users.title'), 'user_id')
             ->addGroupClass('col-md-4')
             ->options([])
         !!}
     @elseIfRole('editor')
+        {{-- Field: Client --}}
+        {!! BootForm::hidden('client_id')->value(getConfig('client', 'id') ?? null) !!}
+
         {{-- Field: User --}}
         {!! BootForm::select(sections('users.title'), 'user_id')
             ->addGroupClass('col-md-4')
