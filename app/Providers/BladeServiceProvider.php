8de0c75d7b10d9b7812b3becfeb6bcfb76a0dce0
daguilarm <damian.aguilarm@gmail.com>
2017-09-11 16:56:42 +0200

    Improvement  and comments update

--- app/Providers/BladeServiceProvider.php
+++ app/Providers/BladeServiceProvider.php
@@ -14,26 +14,54 @@ class BladeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //Blade: role()
+        /*
+        |--------------------------------------------------------------------------
+        |  Role: Check if the user has (at least) this role
+        |  Role() - elseifRole() - endRole()
+        |--------------------------------------------------------------------------
+        */
+        //Blade: Role()
         Blade::directive('Role', function ($role) {
             return "<?php if(auth()->check() && Credentials::role({$role})): ?>";
         });
 
+        //Blade: elseifRole()
+        Blade::directive('elseIfRole', function ($roles) {
+            return "<?php elseif(auth()->check() && Credentials::role({$roles})): ?>";
+        });
+
+        //Blade: endRole()
+        Blade::directive('endRole', function () {
+            return '<?php endif; ?>';
+        });
+
+        /*
+        |--------------------------------------------------------------------------
+        |  Role: Check if the user has all the roles from a list of roles
+        |  hasRoles() - endRoles()
+        |--------------------------------------------------------------------------
+        */
         //Blade: hasRoles()
         Blade::directive('hasRoles', function ($roles) {
             return "<?php if(auth()->check() && Credentials::hasRoles({$roles})): ?>";
         });
-   
+  
+        /*
+        |--------------------------------------------------------------------------
+        |  Role: Check if the user has, at least one, from the list of roles
+        |  hasAnyRoles() - endRoles()
+        |--------------------------------------------------------------------------
+        */ 
         //Blade: hasAnyRoles()
         Blade::directive('hasAnyRoles', function ($roles) {
             return "<?php if(auth()->check() && Credentials::hasAnyRoles({$roles})): ?>";
         });
 
-        //Blade: endRoles()
-        Blade::directive('endRole', function () {
-            return '<?php endif; ?>';
-        });
-
+        /*
+        |--------------------------------------------------------------------------
+        |  Role: End for hasAnyRoles() and hasRoles()
+        |--------------------------------------------------------------------------
+        */ 
         //Blade: endRoles()
         Blade::directive('endRoles', function () {
             return '<?php endif; ?>';
