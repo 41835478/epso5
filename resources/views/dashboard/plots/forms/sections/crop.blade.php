1b633edd643d06db0a8cb43f0a2e8218332a0849
daguilarm <damian.aguilarm@gmail.com>
2017-09-13 15:14:17 +0200

    Refactoring with helpers

--- resources/views/dashboard/plots/forms/sections/crop.blade.php
+++ resources/views/dashboard/plots/forms/sections/crop.blade.php
@@ -5,12 +5,12 @@
 {{-- Crop module --}}
     {{-- Edit case --}}
     @if(isset($data))
-        @include(module_path($data->crop_module), ['moduleName' => $data->crop->crop_name])
+        @include(module_path($data->crop_module), ['cropName' => getCropName($data)])
     {{-- Create case --}}
     @else
         <div id="load-module">@include(module_path('error'))</div>
     @endif
 {{-- Editors and users case --}}
 @else 
-    @include(module_path(getConfig('crop', 'module')), ['moduleName' => getConfig('crop', 'name')])
+    @include(module_path(getModule()), ['cropName' => getCropName()])
 @endRole
