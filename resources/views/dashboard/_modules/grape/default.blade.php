f69da07d8663333e91e52bc23a7bc385a106ea92
daguilarm <damian.aguilarm@gmail.com>
2017-09-13 08:41:46 +0200

    just testing

--- resources/views/dashboard/_modules/grape/default.blade.php
+++ resources/views/dashboard/_modules/grape/default.blade.php
@@ -2,7 +2,7 @@
     {{-- Field: example --}}
     <div class="form-group col-md-3">
         <label class="control-label control-label-required label-required" for="plot_name">Parcela</label>
-        <input type="text" name="plot_example" id="plot_example" class="form-control" required="required">
+        <input type="text" name="plot_example" id="plot_example" class="form-control">
     </div>
 
     {{-- Field: Crop --}}
