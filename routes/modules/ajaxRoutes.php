820a9ae2a979b27a7b2a4c070e3eeac7843ea2e2
daguilarm <damian.aguilarm@gmail.com>
2017-09-14 12:38:54 +0200

    New routes

--- routes/modules/ajaxRoutes.php
+++ routes/modules/ajaxRoutes.php
@@ -5,8 +5,9 @@ Route::group([
         'as' => 'ajax.', 
     ], function () {
         Route::get('ajax/cropVarietyTypes', 'Dashboard\Ajax\CropVarietyTypesController')->name('cropVarietyTypes');  
-        Route::get('ajax/regions', 'Dashboard\Ajax\RegionsController')->name('regions');  
-        Route::get('ajax/users', 'Dashboard\Ajax\UsersController')->name('users'); 
         Route::get('ajax/modules', 'Dashboard\Ajax\ModulesController')->name('modules');  
         Route::get('ajax/modules/load', 'Dashboard\Ajax\ModulesLoadController')->name('modules.load');  
+        Route::get('ajax/regions', 'Dashboard\Ajax\RegionsController')->name('regions');  
+        Route::get('ajax/users', 'Dashboard\Ajax\UsersController')->name('users'); 
+        Route::get('ajax/varieties', 'Dashboard\Ajax\CropVarietiesController')->name('varieties');  
 });
