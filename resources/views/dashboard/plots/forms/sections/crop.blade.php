87119988f01657f88341430ef90669544b7b69b5
daguilarm <damian.aguilarm@gmail.com>
2017-09-15 08:34:13 +0200

    Fixed problem with the module load

--- resources/views/dashboard/plots/forms/sections/crop.blade.php
+++ resources/views/dashboard/plots/forms/sections/crop.blade.php
@@ -1,16 +1,16 @@
 <legend class="title">@lang('sections/plots.crop')</legend>
 
-{{-- Gods and Admins case --}}
-@Role('admin')
 {{-- Crop module --}}
-    {{-- Edit case --}}
-    @if(isset($data))
-        @include(module_path($data->crop_module))
-    {{-- Create case --}}
-    @else
+{{-- Edit case --}}
+@if(isset($data))
+    @include(module_path($data->crop_module))
+{{-- Create case --}}
+@else
+    {{-- Admin and Gods --}}
+    @Role('admin')
         <div id="load-module">@include(module_path('error'))</div>
-    @endif
-{{-- Editors and users case --}}
-@else 
-    @include(module_path(getModule()))
-@endRole
+    {{-- Editors and users --}}
+    @else 
+        @include(module_path(getModule()))
+    @endRole        
+@endif
