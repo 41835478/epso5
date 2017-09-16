c2c24fb55c3f93398c70e37501c9b6a4f36da867
daguilarm <damian.aguilarm@gmail.com>
2017-09-16 13:21:47 +0200

    just testing

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            {{ sections('plots.date') }}
+            {{ print_r(Credentials::config()) }}
         </div>
     </div>
 @endsection
