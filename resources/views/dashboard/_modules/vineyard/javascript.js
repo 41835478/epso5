266f28251ee4a059aed668ca2910d5a64d5f919b
daguilarm <damian.aguilarm@gmail.com>
2017-09-16 12:28:19 +0200

    Fixed problem when no results

--- resources/views/dashboard/_modules/vineyard/javascript.js
+++ resources/views/dashboard/_modules/vineyard/javascript.js
@@ -17,25 +17,29 @@
             var $crop      = $( '#crop_id' );
             //Add loading class 
             $container.empty().addClass( 'loading' );
-            //Get the data via AJAX
-            $.get( window.location.origin + '/dashboard/ajax/varieties', { type: $type.val(), crop: $crop.val() }, 
-                function( data ) {
-                    //Only if there is data
-                    if( data.length > 0 ) {
-                        //First empty option field
-                        $container.append( $( '<option>', { value: '', text : '' } ) );
-                        //Built the select
-                        $.each( data, function( index, element ) {
-                            $container.append( $( '<option>', { 
-                            value: element.id,
-                            text : element.name
-                        }));
-                    });
-                    $container.prop( 'disabled', false ).prop( 'required', true ).removeClass( 'loading' );
-                } else {
-                    $container.prop( 'disabled', true ).prop( 'required', false ).removeClass( 'loading' );
-                }
-            });
+            if($type.val() > 0) {
+                //Get the data via AJAX
+                $.get( window.location.origin + '/dashboard/ajax/varieties', { type: $type.val(), crop: $crop.val() }, 
+                    function( data ) {
+                        //Only if there is data
+                        if( data.length > 0 ) {
+                            //First empty option field
+                            $container.append( $( '<option>', { value: '', text : '' } ) );
+                            //Built the select
+                            $.each( data, function( index, element ) {
+                                $container.append( $( '<option>', { 
+                                    value: element.id,
+                                    text : element.name
+                                }));
+                            });
+                            $container.prop( 'disabled', false ).prop( 'required', true ).removeClass( 'loading' );
+                        } else {
+                            $container.prop( 'disabled', true ).prop( 'required', false ).removeClass( 'loading' );
+                        }
+                });
+            } else {
+                $container.removeClass( 'loading' );
+            }
         });
     }
 });
