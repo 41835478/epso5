8a9a33ad64df0dfe4b275670a7b0c17d42996f97
daguilarm <damian.aguilarm@gmail.com>
2017-09-25 20:49:46 +0200

    New file

---
+++ resources/lang/es/errors.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Geolocations
+    |--------------------------------------------------------------------------
+    */
+    'errorValidation' => [
+        'Se ha producido un error interno.',
+        'El sistema ha enviado un aviso al administrador con todos los detalles del mismo.',
+        'Estamos trabajando en solucionar el problema lo antes posible.',
+        'Si desea contactar directamente con nosotros, por favor, hágalo mediante el email: <b>' . config('mail.from.address') . '</b>'
+    ],
+];
