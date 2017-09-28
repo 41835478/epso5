ded815232dae5debfe1c67a2b0ebac41c59e4050
daguilarm <damian.aguilarm@gmail.com>
2017-09-28 19:21:14 +0200

    Add new helpers

--- tests/Helpers/PlotHelpers.php
+++ tests/Helpers/PlotHelpers.php
@@ -10,6 +10,10 @@ trait PlotHelpers
     protected $firstPlot;
     protected $lastPlot;
     protected $makePlot;
+    protected $whereClientIs;
+    protected $whereClientIsNot;
+    protected $whereUserIs;
+    protected $whereUserIsNot;
 
     /**
      * Create an item and storing it
@@ -64,12 +68,81 @@ trait PlotHelpers
     }
 
     /**
+     * Where client is
+     *
+     * @return Object
+     */
+    public function whereClientIs($plot) : Plot
+    {
+        if($this->whereClientIs) {
+            return $this->whereClientIs;
+        }
+        return $this->whereClientIs = Plot::where('client_id', $plot->client_id)->inRandomOrder()->first();
+    }
+
+    /**
+     * Where client is not
+     *
+     * @return Object
+     */
+    public function whereClientIsNot($plot) : Plot
+    {
+        if($this->whereClientIsNot) {
+            return $this->whereClientIsNot;
+        }
+        return $this->whereClientIsNot = Plot::where('client_id', '!=', $plot->client_id)->inRandomOrder()->first();
+    }
+
+    /**
+     * Where client is not
+     *
+     * @return Object
+     */
+    public function whereUserIs($user) : Plot
+    {
+        if($this->whereUserIs) {
+            return $this->whereUserIs;
+        }
+        return $this->whereUserIs = Plot::where('user_id', $user->id)->inRandomOrder()->first();
+    }
+
+    /**
+     * Where client is not
+     *
+     * @return Object
+     */
+    public function whereUserIsNot($user) : Plot
+    {
+        if($this->whereUserIsNot) {
+            return $this->whereUserIsNot;
+        }
+        return $this->whereUserIsNot = Plot::where('user_id', '!=', $user->id)->inRandomOrder()->first();
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | Helpers
+    |--------------------------------------------------------------------------
+    */
+
+    /**
      * Format the framework value
      *
      * @return string
      */
-    public function framework($string)
+    protected function framework($string)
     {
         return substr($string, 0 , 1) . 'x' . substr($string, 1 , 1);
+    }
+
+    /**
+     * Format the framework value
+     *
+     * @return string
+     */
+    protected function area($plot)
+    {
+        $area = ($plot->plot_area / 100) * $plot->plot_percent_cultivated_land;
+            return number_format($area, 2, '.', '');
     }
 }
