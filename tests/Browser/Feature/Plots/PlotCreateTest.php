fc60cc2bda156822b43bcf2ed09e6f73ab5612ea
daguilarm <damian.aguilarm@gmail.com>
2017-09-28 11:53:52 +0200

    Created plot test

--- tests/Browser/Feature/Plots/PlotCreateTest.php
+++ tests/Browser/Feature/Plots/PlotCreateTest.php
@@ -49,38 +49,28 @@ class PlotCreateTest extends DuskTestCase
                 ->click('#map')
                 ->press('#button-create-submit');
                 //Step 2: create a plot
-                $browser;
+                $browser->assertMissing('plot_crop_type')
+                ->assertMissing('crop_variety_id')
+                ->assertMissing('pattern_id')
+                ->assertMissing('plot_quantity')
+                ->assertMissing('crop_training')
+                //Administration
+                ->select('client_id', 1)->pause(500)
+                ->select('user_id', 1)
+                //Plot
+                ->type('plot_name', $this->makePlot()->plot_name)
+                ->type('plot_framework', $this->makePlot()->plot_framework)
+                ->type('plot_area', $this->makePlot()->plot_area)
+                ->type('plot_percent_cultivated_land', $this->makePlot()->plot_percent_cultivated_land)
+                ->type('plot_start_date', $this->makePlot()->plot_start_date)
+                ->select('crop_variety_id', 1)
+                ->select('pattern_id', 1)
+                ->type('crop_quantity', $this->makePlot()->crop_quantity)
+                ->select('crop_training', 1)
+                ->press(trans('buttons.new'))
+                ->assertSee(__('The item has been create successfuly'));
         });
     }
-
-    // public function test_god_can_create_a_plot()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs($god = $this->createGod())
-    //             ->visit($this->pathToList)
-    //             ->click('#button-config')
-    //             ->click('#button-create-link')
-    //             ->assertPathIs($this->pathToCreate)
-    //             ->assertMissing('plot_crop_type')
-    //             ->assertMissing('crop_variety_id')
-    //             ->assertMissing('pattern_id')
-    //             ->assertMissing('plot_quantity')
-    //             ->assertMissing('crop_training')
-    //             ->select('client_id', 1)
-    //             ->select('user_id', 1)
-    //             ->pause(500)
-    //             ->select('plot_crop_type', 1)
-    //             ->pause(500)
-    //             ->select('crop_variety_id', 1)
-    //             ->select('pattern_id', 1)
-    //             ->type('plot_quantity', 1000)
-    //             ->select('crop_training', 1);
-    //             // ->type('plot_name', $this->makePlot()->plot_name)
-    //             // ->type('plot_description', $this->makePlot()->plot_description)
-    //             // ->press(trans('buttons.new'))
-    //             // ->assertSee(__('The item has been create successfuly'));
-    //     });
-    //}
 
     public function test_admin_can_create_plot()
     {
