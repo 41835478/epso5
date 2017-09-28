e8f841ce4c14f5dbc23f500f8c9be934217564bc
daguilarm <damian.aguilarm@gmail.com>
2017-09-28 19:21:26 +0200

    Finished the test

--- tests/Browser/Feature/Plots/PlotUpdateTest.php
+++ tests/Browser/Feature/Plots/PlotUpdateTest.php
@@ -12,7 +12,8 @@ class PlotUpdateTest extends DuskTestCase
 {
     use PlotHelpers;
     
-    protected $route = 'dashboard.user.plots.edit';
+    protected $route        = 'dashboard.user.plots.edit';
+    protected $dashboard    = '/dashboard';
 
     /*
     |--------------------------------------------------------------------------
@@ -34,22 +35,72 @@ class PlotUpdateTest extends DuskTestCase
                 ->select('crop_variety_id', $this->makePlot()->crop_variety_id)
                 ->select('pattern_id', $this->makePlot()->pattern_id)
                 ->type('crop_quantity', $this->makePlot()->crop_quantity)
-                ->select('crop_training', 2)
+                ->select('crop_training', $this->makePlot()->crop_training)
                 ->press(trans('buttons.edit'))
-                ->assertSee(__('The items has been updated successfuly'));
+                ->assertSee(__('The items has been updated successfuly'))
+                ->assertDontSee(__('Your are not authorized to access this section'));
         });
 
         $this->assertDatabaseHas('plots', [
             'plot_name'                     => $this->makePlot()->plot_name,
             'plot_framework'                => $this->framework($this->makePlot()->plot_framework),
             'plot_area'                     => $this->makePlot()->plot_area,
-            'plot_real_area'                => (($this->makePlot()->plot_area / 100) * $this->makePlot()->plot_percent_cultivated_land),
+            'plot_real_area'                => $this->area($this->makePlot()),
             'plot_percent_cultivated_land'  => $this->makePlot()->plot_percent_cultivated_land,
             'plot_start_date'               => date_to_db($this->makePlot()->plot_start_date),
             'crop_variety_id'               => $this->makePlot()->crop_variety_id,
             'pattern_id'                    => $this->makePlot()->pattern_id,
             'crop_quantity'                 => $this->makePlot()->crop_quantity,
-            'crop_training'                 => 2,
         ]);
+    }
+
+    public function test_editor_can_update_a_plot()
+    {
+        $plot = $this->whereClientIs($this->createEditor());
+
+        $this->browse(function (Browser $browser) use ($plot) {
+            $browser->loginAs($editor = $this->createEditor())
+                ->visitRoute($this->route, $plot->id)
+                ->assertMissing('#client_id')
+                ->press(trans('buttons.edit'))
+                ->assertSee(__('The items has been updated successfuly'))
+                ->assertDontSee(__('Your are not authorized to access this section'));
+        });
+    }
+
+    public function test_editor_cant_update_a_plot_fron_other_client()
+    {
+        $plot = $this->whereClientIsNot($this->createEditor());
+
+        $this->browse(function (Browser $browser) use ($plot) {
+            $browser->loginAs($editor = $this->createEditor())
+                ->visitRoute($this->route, $plot->id)
+                ->assertPathIs($this->dashboard);
+        });
+    }
+
+    public function test_user_can_update_a_plot()
+    {
+        $plot = $this->whereUserIs($this->createUserBase());
+
+        $this->browse(function (Browser $browser) use ($plot) {
+            $browser->loginAs($user = $this->createUserBase())
+                ->visitRoute($this->route, $plot->id)
+                ->assertMissing('#client_id')
+                ->press(trans('buttons.edit'))
+                ->assertSee(__('The items has been updated successfuly'))
+                ->assertDontSee(__('Your are not authorized to access this section'));
+        });
+    }
+
+    public function test_user_can_update_a_plot_from_other_user()
+    {
+        $plot = $this->whereUserIsNot($this->createUserBase());
+
+        $this->browse(function (Browser $browser) use ($plot) {
+            $browser->loginAs($user = $this->createUserBase())
+                ->visitRoute($this->route, $plot->id)
+                ->assertPathIs($this->dashboard);
+        });
     }
 }
