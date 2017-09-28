99aab78c8a1a2fd0ede4ecc206e46a7b5405f3db
daguilarm <damian.aguilarm@gmail.com>
2017-09-28 15:43:07 +0200

    Finished admin test

--- tests/Browser/Feature/Plots/PlotUpdateTest.php
+++ tests/Browser/Feature/Plots/PlotUpdateTest.php
@@ -19,20 +19,37 @@ class PlotUpdateTest extends DuskTestCase
     | Update clients
     |--------------------------------------------------------------------------
     */
-    // public function test_admin_can_update_a_plot()
-    // {
-    //     $this->browse(function (Browser $browser) {
-    //         $browser->loginAs($admin = $this->createAdmin())
-    //             ->visitRoute($this->route, $this->lastPlot()->id)
-    //             ->type('plot_name', $this->makePlot()->plot_name)
-    //             // ->type('plot_description', $this->makePlot()->plot_description)
-    //             ->press(trans('buttons.edit'))
-    //             ->assertSee(__('The items has been updated successfuly'));
-    //     });
+    public function test_admin_can_update_a_plot()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->loginAs($admin = $this->createAdmin())
+                ->visitRoute($this->route, $this->lastPlot()->id)
+                ->assertMissing('#client_id')
+                ->select('user_id', $this->makePlot()->user_id)
+                ->type('plot_name', $this->makePlot()->plot_name)
+                ->type('plot_framework', $this->makePlot()->plot_framework)
+                ->type('plot_area', $this->makePlot()->plot_area)
+                ->type('plot_percent_cultivated_land', $this->makePlot()->plot_percent_cultivated_land)
+                ->type('plot_start_date', $this->makePlot()->plot_start_date)
+                ->select('crop_variety_id', $this->makePlot()->crop_variety_id)
+                ->select('pattern_id', $this->makePlot()->pattern_id)
+                ->type('crop_quantity', $this->makePlot()->crop_quantity)
+                ->select('crop_training', 2)
+                ->press(trans('buttons.edit'))
+                ->assertSee(__('The items has been updated successfuly'));
+        });
 
-    //     $this->assertDatabaseHas('plots', [
-    //         'plot_name'           => $this->makePlot()->plot_name,
-    //         // 'plot_description'    => $this->makePlot()->plot_description,
-    //     ]);
-    // }
+        $this->assertDatabaseHas('plots', [
+            'plot_name'                     => $this->makePlot()->plot_name,
+            'plot_framework'                => $this->framework($this->makePlot()->plot_framework),
+            'plot_area'                     => $this->makePlot()->plot_area,
+            'plot_real_area'                => (($this->makePlot()->plot_area / 100) * $this->makePlot()->plot_percent_cultivated_land),
+            'plot_percent_cultivated_land'  => $this->makePlot()->plot_percent_cultivated_land,
+            'plot_start_date'               => date_to_db($this->makePlot()->plot_start_date),
+            'crop_variety_id'               => $this->makePlot()->crop_variety_id,
+            'pattern_id'                    => $this->makePlot()->pattern_id,
+            'crop_quantity'                 => $this->makePlot()->crop_quantity,
+            'crop_training'                 => 2,
+        ]);
+    }
 }
