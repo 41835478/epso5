1f9497c4c22e5577d11bc1bde37760fe83db52ec
daguilarm <damian.aguilarm@gmail.com>
2017-09-29 09:46:21 +0200

    Removed route

--- routes/modules/ajaxRoutes.php
+++ routes/modules/ajaxRoutes.php
@@ -4,7 +4,6 @@
 Route::group([
         'as' => 'ajax.', 
     ], function () {
-        Route::get('ajax/cropVarietyTypes', 'Dashboard\Ajax\CropVarietyTypesController')->name('cropVarietyTypes');  
         Route::get('ajax/modules', 'Dashboard\Ajax\ModulesController')->name('modules');  
         Route::get('ajax/modules/load', 'Dashboard\Ajax\ModulesLoadController')->name('modules.load');  
         Route::get('ajax/cities', 'Dashboard\Ajax\CitiesController')->name('cities');  
