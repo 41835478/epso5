2282a8fb0af4d14329a2b83b71576b0c5c108d57
daguilarm <damian.aguilarm@gmail.com>
2017-09-29 15:17:02 +0200

    Updated fields

--- database/migrations/2017_09_29_150009_create_edaphologies_table.php
+++ database/migrations/2017_09_29_150009_create_edaphologies_table.php
@@ -15,7 +15,27 @@ class CreateEdaphologiesTable extends Migration
     {
         Schema::create('edaphologies', function (Blueprint $table) {
             $table->increments('id')->unsigned();
-            //$table->softDeletes();
+            $table->integer('reference')->unsigned();
+            $table->integer('plot_id')->unsigned()->index();
+            $table->integer('edaphology_level', 1)->default(1)->comment('1 for primary sample. 2 for secundary sample');
+            $table->decimal('edaphology_lat', 10, 6)->nullable();
+            $table->decimal('edaphology_lng', 10, 6)->nullable();
+            $table->string('edaphology_name')->nullable()->comment('If the sample was taken from a specific place...');
+            $table->text('edaphology_observations')->nullable();
+            $table->integer('edaphology_aggregate_stability')->unsigned()->default(0)->comment('Aggregate stability in %');
+            $table->integer('edaphology_coarse_elements')->unsigned()->default(0)->comment('Coarse elements in %');
+            $table->integer('edaphology_sand')->unsigned()->default(0)->comment('Sand in %');
+            $table->integer('edaphology_silt')->unsigned()->default(0)->comment('Silt in %');
+            $table->integer('edaphology_clay')->unsigned()->default(0)->comment('Clay in %');
+            $table->integer('edaphology_ph')->unsigned()->default(0)->comment('Just the pH');
+            $table->integer('edaphology_electric_conductivity')->unsigned()->default(0)->comment('Electric conductivity in dS/m');
+            $table->integer('edaphology_calcium_carbonate_equivalent')->unsigned()->default(0)->comment('Calcium carbonate equivalent in %');
+            $table->integer('edaphology_active_lime')->unsigned()->default(0)->comment('Active lime (caliza activa) %');
+            $table->integer('edaphology_total_organic_matter')->unsigned()->default(0)->comment('Organic matter in %');
+            $table->integer('edaphology_organic_carbon')->unsigned()->default(0)->comment('Organic carbon in %');
+            $table->integer('edaphology_cation_exchange')->unsigned()->default(0)->comment('Cation exchange in CIC');
+            $table->string('edaphology_texture')->nullable();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
