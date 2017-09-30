f80431d9edf7cf90f94c8d5c26c9b95d0568acc5
daguilarm <damian.aguilarm@gmail.com>
2017-09-30 15:06:49 +0200

    Added new filter

--- app/Repositories/Edaphologies/EdaphologiesRepository.php
+++ app/Repositories/Edaphologies/EdaphologiesRepository.php
@@ -2,20 +2,23 @@
 
 namespace App\Repositories\Edaphologies;
 
-use App\Repositories\Repository;
-use App\Repositories\Edaphologies\Traits\EdaphologiesHelpers;
 use App\Repositories\Edaphologies\Edaphology;
-//use DB;
+use App\Repositories\Edaphologies\Traits\EdaphologiesHelpers;
+use App\Repositories\Plots\PlotsRepository;
+use App\Repositories\Repository;
+use Credentials, Schema;
 
 class EdaphologiesRepository extends Repository
 {
     use EdaphologiesHelpers;
 
     protected $model;
+    protected $plots;
 
-    public function __construct(Edaphology $model)
+    public function __construct(Edaphology $model, PlotsRepository $plots)
     {
         $this->model = $model;
+        $this->plots = $plots;
     }
 
     /**
@@ -29,9 +32,42 @@ class EdaphologiesRepository extends Repository
     {
         //The query
         $query = $this->model
-            ->select($columns)->where('plot_id', $value);
+            ->select($columns)->where('plot_id', $value->id);
                 //The filters
-                return $this->filterByRole($query, $table, $userNull);
+                return $this->filterByRole($query, $table, $value);
     }
 
+    /**
+     * Set value from the controller to the Datatable constructor
+     * @param string $query
+     * @param string $table
+     * @param string $userNull when user = null
+     * @return object
+     */
+    protected function filterByRole($query, $table, $userNull = false)
+    {
+        //Just in case we need to add de table name for avoid ambiguous row names
+        $tableWithDot = $table 
+            ? $table . '.' 
+            : '';
+        //Get the plot 
+        $user = $userNull->user_id;
+        //Conditional query
+        return 
+            $query->when(Credentials::maxRole() === 'god' || Credentials::maxRole() === 'admin', function ($query) {
+                return $query;
+            })
+            ->when(Credentials::maxRole() === 'editor', function ($query) use ($tableWithDot) {
+                return $query->where($tableWithDot . 'client_id', Credentials::client());
+            })
+            ->when(Credentials::maxRole() === 'user', function ($query) use ($table, $tableWithDot, $user) {
+                //Filter by user
+                if(Schema::hasColumn($table, 'user_id')) {
+                    return $query->where($tableWithDot . 'user_id', Credentials::id());
+                }    
+                //Filter by user throw plot table      
+                $listOfPlots = $this->plots->listsByUser($user);
+                    return $query->whereIn('plot_id', $listOfPlots);
+            });
+    }
 }
