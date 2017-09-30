b3aeaf40bca5b0df732898a0d71de527df38ff77
daguilarm <damian.aguilarm@gmail.com>
2017-09-30 12:30:59 +0200

    Removed a duplicated query

--- app/Repositories/_Traits/Role.php
+++ app/Repositories/_Traits/Role.php
@@ -19,6 +19,8 @@ trait Role
         $tableWithDot = $table 
             ? $table . '.' 
             : '';
+        //Check if table exists 
+        $checkTable = Schema::hasColumn($table, 'user_id');
         //Conditional query
         return 
             $query->when(Credentials::maxRole() === 'god' || Credentials::maxRole() === 'admin', function ($query) {
@@ -27,10 +29,10 @@ trait Role
             ->when(Credentials::maxRole() === 'editor', function ($query) use ($tableWithDot) {
                 return $query->where($tableWithDot . 'client_id', Credentials::client());
             })
-            ->when(Schema::hasColumn($table, 'user_id') && Credentials::maxRole() === 'user', function ($query) use ($tableWithDot) {
+            ->when($checkTable && Credentials::maxRole() === 'user', function ($query) use ($tableWithDot) {
                 return $query->where($tableWithDot . 'user_id', Credentials::id());
             })
-            ->when(Schema::hasColumn($table, 'user_id') && $userNull, function ($query) {
+            ->when($checkTable && $userNull, function ($query) {
                 return $query->where('user_id', null);
             });
     }
