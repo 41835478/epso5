746fc098dfc63e6773a0106e4ef47bf37ba717f9
daguilarm <damian.aguilarm@gmail.com>
2017-09-30 12:20:37 +0200

    Just renamed variable

--- app/Repositories/_Traits/Role.php
+++ app/Repositories/_Traits/Role.php
@@ -16,17 +16,19 @@ trait Role
     protected function filterByRole($query, $table, $userNull = false)
     {
         //Just in case we need to add de table name for avoid ambiguous row names
-        $tableRoute = $table ? $table . '.' : '';
+        $tableWithDot = $table 
+            ? $table . '.' 
+            : '';
         //Conditional query
         return 
             $query->when(Credentials::maxRole() === 'god' || Credentials::maxRole() === 'admin', function ($query) {
                 return $query;
             })
-            ->when(Credentials::maxRole() === 'editor', function ($query) use ($tableRoute) {
-                return $query->where($tableRoute . 'client_id', Credentials::client());
+            ->when(Credentials::maxRole() === 'editor', function ($query) use ($tableWithDot) {
+                return $query->where($tableWithDot . 'client_id', Credentials::client());
             })
-            ->when(Schema::hasColumn($table, 'user_id') && Credentials::maxRole() === 'user', function ($query) use ($tableRoute) {
-                return $query->where($tableRoute . 'user_id', Credentials::id());
+            ->when(Schema::hasColumn($table, 'user_id') && Credentials::maxRole() === 'user', function ($query) use ($tableWithDot) {
+                return $query->where($tableWithDot . 'user_id', Credentials::id());
             })
             ->when(Schema::hasColumn($table, 'user_id') && $userNull, function ($query) {
                 return $query->where('user_id', null);
