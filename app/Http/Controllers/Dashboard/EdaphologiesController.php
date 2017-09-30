f636550f3eede91da28c66f96a5981a9b704fc7e
daguilarm <damian.aguilarm@gmail.com>
2017-09-30 12:19:12 +0200

    Renamed

--- app/Http/Controllers/Dashboard/EdaphologiesController.php
+++ app/Http/Controllers/Dashboard/EdaphologiesController.php
@@ -51,7 +51,7 @@ class EdaphologiesController extends DashboardController
             return $this->table
                 //Customize the action for datatables [dashboard/_components/actions]
                 ->setValue([
-                    'plot' => $plot
+                    'id' => $plot->id
                 ])
                 ->render(dashboard_path($this->section . '.index'), compact('plot'));
     }
