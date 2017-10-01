150d6f9957e703f0ae52dce5651858e033964aaf
daguilarm <damian.aguilarm@gmail.com>
2017-10-01 12:26:25 +0200

    Added multiple markers to maps... when available

--- public/js/maps.js
+++ public/js/maps.js
@@ -35765,17 +35765,25 @@ var lng = window.currentLng;
 var zoon = 18;
 var container = 'simpleMap';
 var marker = null;
+var coordenates = coordenates || null;
 
 /**
  * Generate the map
  */
 if (typeof L !== 'undefined' && lat && lng) {
-  /////////////////////
-  // Dafault map
-  /////////////////////  
-  var map = __WEBPACK_IMPORTED_MODULE_0__helpers_maps_js__["a" /* default */].generateMap(lat, lng, zoon, container);
-  //Generate the marker
-  marker = L.marker(new L.LatLng(lat, lng)).addTo(map);
+    /////////////////////
+    // Dafault map
+    /////////////////////  
+    var map = __WEBPACK_IMPORTED_MODULE_0__helpers_maps_js__["a" /* default */].generateMap(lat, lng, zoon, container);
+    //Generate the marker
+    marker = L.marker(new L.LatLng(lat, lng)).addTo(map);
+    //Coordenadas 
+    if (coordenates) {
+        //Generate the marker
+        for (var i = 0; i < coordenates.length; i++) {
+            marker = new L.marker([coordenates[i][1], coordenates[i][2]]).bindPopup("Latitud: " + coordenates[i][1] + " - Longitud: " + coordenates[i][2]).addTo(map);
+        }
+    }
 }
 
 /***/ }),
--- resources/assets/js/dashboard/jquery/maps.js
+++ resources/assets/js/dashboard/jquery/maps.js
@@ -9,6 +9,7 @@
  var zoon       = 18;
  var container  = 'simpleMap';
  var marker     = null;
+ var coordenates = coordenates || null;
 
  /**
   * Generate the map
@@ -20,4 +21,13 @@ if( typeof( L ) !== 'undefined' && lat && lng) {
     var map = maps.generateMap(lat, lng, zoon, container);
     //Generate the marker
     marker  = L.marker( new L.LatLng( lat, lng ) ).addTo( map );
+    //Coordenadas 
+    if(coordenates) {
+        //Generate the marker
+        for (var i = 0; i < coordenates.length; i++) {
+            marker = new L.marker([coordenates[i][1],coordenates[i][2]])
+                .bindPopup("Latitud: " + coordenates[i][1] + " - Longitud: " + coordenates[i][2])
+                .addTo(map);
+        }
+    }
 }
