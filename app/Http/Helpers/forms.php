c734a7fea18013bac6a45f9c7150004717ed4114
daguilarm <damian.aguilarm@gmail.com>
2017-10-02 15:43:37 +0200

    New helper

--- app/Http/Helpers/forms.php
+++ app/Http/Helpers/forms.php
@@ -12,3 +12,46 @@ if (!function_exists('setOptions')) {
         return $firstOptionBlank ? ['' => ''] + $options : $options;
     }
 }
+
+/**
+ * Return a status field for a form
+ * 
+ * @param string $status
+ * @return string
+ */
+if (!function_exists('formStatus')) {
+    function formStatus($status = null)
+    {
+        if($status === 'required') {
+            return ' ' . 'required="required"';
+        }
+        if($status === 'disabled') {
+            return ' ' . 'disabled="disabled"';
+        }
+        if($status === 'readonly') {
+            return ' ' . 'readonly="readonly"';
+        }
+        if($status === 'checked') {
+            return ' ' . 'checked="checked"';
+        }
+        if($status === 'selected') {
+            return ' ' . 'selected';
+        }
+        return '';
+    }
+}
+
+/**
+ * Return a status field for a form
+ * 
+ * @param string $class
+ * @return string
+ */
+if (!function_exists('formClass')) {
+    function formClass($class = null)
+    {
+        return $class 
+            ? ' ' . $class 
+            : '';
+    }
+}
