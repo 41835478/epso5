3183c507b616874ffff0aa53fbb65220949a6925
daguilarm <damian.aguilarm@gmail.com>
2017-10-02 15:44:37 +0200

    Added new include

--- resources/views/dashboard/edaphologies/forms/builder.blade.php
+++ resources/views/dashboard/edaphologies/forms/builder.blade.php
@@ -1,3 +1,7 @@
-{{-- All the personal information --}}
-<legend class="title">@lang('sections/edaphologies.info')</legend>
+{{-- All the general information --}}
+<legend class="title">@lang('sections/edaphologies.info:sample')</legend>
 @include(dashboard_path('edaphologies.forms.sections.default'))
+
+{{-- All the analysis information --}}
+<legend class="title">@lang('sections/edaphologies.info')</legend>
+@include(dashboard_path('edaphologies.forms.sections.analysis'))
