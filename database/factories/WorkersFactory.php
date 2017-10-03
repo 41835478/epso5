d58b8c55d149616563187fa08f5b0784078cdda1
daguilarm <damian.aguilarm@gmail.com>
2017-10-03 10:36:44 +0200

    New file

---
+++ database/factories/WorkersFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+use App\Repositories\Workers\Worker;
+
+/*
+|--------------------------------------------------------------------------
+| Factory for Worker
+|--------------------------------------------------------------------------
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Worker::class, function (Faker\Generator $faker) {
+    return [
+        'user_id'                   => 1,
+        'client_id'                 => 1,
+        'worker_name'               => $faker->name,
+        'worker_nif'                => $faker->swiftBicNumber,
+        'worker_ropo'               => $faker->swiftBicNumber,
+        'worker_ropo_date'          => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'worker_ropo_level'         => rand(1, 4),
+        'worker_start'              => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'worker_observations'       => $faker->sentence($nbWords = 10, $variableNbWords = true),
+    ];
+});
