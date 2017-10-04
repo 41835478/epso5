afeef01f942481a8ef6364d31018ce5b7f6cc722
daguilarm <damian.aguilarm@gmail.com>
2017-10-04 22:07:23 +0200

    New factory

---
+++ database/factories/MachinesFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Repositories\Machines\Machine;
+
+/*
+|--------------------------------------------------------------------------
+| Factory for Machine
+|--------------------------------------------------------------------------
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+$factory->define(Machine::class, function (Faker\Generator $faker) {
+    return [
+        'user_id'                       => rand(1, 10),
+        'machine_equipment_name'        => $faker->company,
+        'machine_brand'                 => $faker->company,
+        'machine_model'                 => $faker->company,
+        'machine_date'                  => $faker->date($format = 'd/m/Y', $max = 'now'),
+        'machine_inspection'            => $faker->date($format = 'd/m/Y', $max = 'now'),
+        'machine_next_inspection'       => $faker->date($format = 'd/m/Y', $max = 'now'),
+        'machine_observations'          => $faker->sentence($nbWords = 10, $variableNbWords = true),
+    ];
+});
