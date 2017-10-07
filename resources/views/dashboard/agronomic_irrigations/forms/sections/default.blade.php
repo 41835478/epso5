2bd85a5384a9706baea67916fded25f71428a171
daguilarm <damian.aguilarm@gmail.com>
2017-10-07 16:44:33 +0200

    Refactoring with macros

--- resources/views/dashboard/agronomic_irrigations/forms/sections/default.blade.php
+++ resources/views/dashboard/agronomic_irrigations/forms/sections/default.blade.php
@@ -6,41 +6,15 @@
     {{-- Row id --}}
     {!! BootForm::hidden('row_id')->value($data->id ?? null) !!}
 
-    {{-- Field:  --}}
-    {!! BootForm::text(trans('persona.name'), 'name')
-        ->addGroupClass('col-md-4')
-        ->autofocus()
-        ->required()
-    !!}
+    {{-- Field: Application date --}}
+    {!!  Form::agronomicDate() !!}
 
-    {{-- Select --}}
-    {!! BootForm::select(trans('persona.locale'), 'locale')
-        ->addGroupClass('col-md-2')
-        ->options(select('locale') ?? [])
-        ->defaultValue('es')
-        ->required()
-    !!}
+    {{-- Field: Quantity  --}}
+    {!! Form::agronomicQuantity() !!}
 
-    {{-- Addon --}}
-    {!! BootForm::InputGroup(trans('base.date'), 'agronomic_date')
-        ->addGroupClass('col-md-2')
-        ->addClass('date')
-        ->afterAddon(icon('calendar'))
-        ->required() 
-    !!}
+    {{-- Field: Quantity units --}}
+    {!! Form::agronomicUnits($section) !!}
 
-    {{-- textarea --}}
-    {!! BootForm::textarea(trans('base.observations'), 'edaphology_observations')
-        ->addGroupClass('col-md-12')
-        ->rows(5)
-        ->maxlength(250)
-    !!}
-    <div class="ml-3" id="textareaAlert-edaphology_observations"></div>
-
-    {{-- Field: Conditional role --}}
-    {{-- @Role('admin')
-        //
-    @else 
-        //
-    @endRoles --}}
+    {{-- Field: observations --}}
+    {!! Form::autoTextArea('agronomic_observations') !!}
 </div>
