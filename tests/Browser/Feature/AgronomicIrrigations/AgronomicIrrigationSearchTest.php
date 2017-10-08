32f6bd4bbac9ae4cd1bed508cbbcaae96bb1d4f1
daguilarm <damian.aguilarm@gmail.com>
2017-10-08 11:01:01 +0200

    Added new method

--- tests/Browser/Feature/AgronomicIrrigations/AgronomicIrrigationSearchTest.php
+++ tests/Browser/Feature/AgronomicIrrigations/AgronomicIrrigationSearchTest.php
@@ -7,10 +7,11 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use Tests\Helpers\AgronomicIrrigationHelpers;
+use Tests\Helpers\CropHelpers;
 
 class AgronomicIrrigationSearchTest extends DuskTestCase
 {
-    use AgronomicIrrigationHelpers;
+    use AgronomicIrrigationHelpers, CropHelpers;
     
     protected $path = '/dashboard/agronomic_irrigations';
 
@@ -24,20 +25,40 @@ class AgronomicIrrigationSearchTest extends DuskTestCase
         $this->browse(function (Browser $browser) {
             $browser->loginAs($god = $this->createGod())
                 ->visit($this->path)
-                ->type('search_name', $this->lastAgronomicIrrigation()->agronomicirrigation_name)
+                ->type('search_crop', $this->firstCrop()->crop_name)
                 ->pause(500)
                 ->with('.table', function ($table) {
-                    $table->assertSee($this->lastAgronomicIrrigation()->agronomicirrigation_name);
-                    $table->assertDontSee($this->firstAgronomicIrrigation()->agronomicirrigation_name);
+                    $table->assertSee($this->firstCrop()->crop_name);
+                    $table->assertDontSee($this->secondCrop()->crop_name);
                 });
 
             $browser->click('.buttons-reset')
                 ->pause(500)
-                ->type('search_id', $this->lastAgronomicIrrigation()->id)
+                ->type('search_client', $this->lastAgronomicIrrigation()->client->client_name)
+                ->pause(1000)
+                ->with('.table', function ($table) {
+                    $clientId = $this->lastAgronomicIrrigation()->client_id;
+                    $table->assertSee($this->lastAgronomicIrrigation()->client->client_name);
+                    $table->assertDontSee($this->notThisClientAgronomicIrrigation($clientId)->client->client_name);
+                });
+            
+            $browser->click('.buttons-reset')
+                ->pause(500)
+                ->type('search_user', parent::reduceText($this->lastAgronomicIrrigation()->user->name))
+                ->pause(1000)
+                ->with('.table', function ($table) {
+                    $clientId = $this->lastAgronomicIrrigation()->client_id;
+                    $table->assertSee(parent::reduceText($this->lastAgronomicIrrigation()->user->name));
+                    $table->assertDontSee(parent::reduceText($this->notThisClientAgronomicIrrigation($clientId)->user->name));
+                });
+
+            $browser->click('.buttons-reset')
+                ->pause(500)
+                ->type('search_plot', parent::reduceText($this->lastAgronomicIrrigation()->plot->plot_name))
                 ->pause(500)
                 ->with('.table', function ($table) {
-                    $table->assertSee($this->lastAgronomicIrrigation()->agronomicirrigation_name);
-                    $table->assertDontSee($this->firstAgronomicIrrigation()->agronomicirrigation_name);
+                    $table->assertSee(parent::reduceText($this->lastAgronomicIrrigation()->plot->plot_name));
+                    $table->assertDontSee(parent::reduceText($this->firstAgronomicIrrigation()->plot->plot_name));
                 });
         });
     }
