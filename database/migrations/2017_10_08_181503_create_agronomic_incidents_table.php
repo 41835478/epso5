13341b347ff96c6c0aca459910bf556d3d7f6a4f
daguilarm <damian.aguilarm@gmail.com>
2017-10-08 20:11:37 +0200

    Created db

--- database/migrations/2017_10_08_181503_create_agronomic_incidents_table.php
+++ database/migrations/2017_10_08_181503_create_agronomic_incidents_table.php
@@ -15,6 +15,12 @@ class CreateAgronomicIncidentsTable extends Migration
     {
         Schema::create('agronomic_incidents', function (Blueprint $table) {
             $table->increments('id')->unsigned();
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('client_id')->unsigned()->index();
+            $table->integer('plot_id')->unsigned()->index();
+            $table->integer('crop_id')->unsigned()->index();
+            $table->date('agronomic_date')->comment("Date of application");
+            $table->text('agronomic_observations')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
