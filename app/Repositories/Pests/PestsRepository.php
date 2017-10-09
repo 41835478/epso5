087784521362c99ea41dad80dd522f7d98cf5e79
daguilarm <damian.aguilarm@gmail.com>
2017-10-09 21:58:39 +0200

    New methods

--- app/Repositories/Pests/PestsRepository.php
+++ app/Repositories/Pests/PestsRepository.php
@@ -5,7 +5,7 @@ namespace App\Repositories\Pests;
 use App\Repositories\Repository;
 use App\Repositories\Pests\Traits\PestsHelpers;
 use App\Repositories\Pests\Pest;
-//use DB;
+use Credentials;
 
 class PestsRepository extends Repository
 {
@@ -16,5 +16,33 @@ class PestsRepository extends Repository
     public function __construct(Pest $model)
     {
         $this->model = $model;
+    }
+
+    /**
+     * Get all the fields from storage
+     * @param integer $cropId
+     * @param array $columns
+     * 
+     * @return collection
+     */
+    public function listByCrop($cropId, array $columns = ['id', 'pest_name'])
+    {
+        return $this->model
+            ->whereCropId($cropId)
+            ->pluck($columns[1], $columns[0])
+            ->toArray();
+    }
+
+    /**
+     * Get all the fields from storage
+     * @param integer $cropId
+     * 
+     * @return collection
+     */
+    public function listByCropAndRole()
+    {
+        return Credentials::isOnlyRole('user') 
+            ? self::listByCrop(getCropId()) 
+            : null;
     }
 }
