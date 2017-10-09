a24d36080ac4426c825d515803ccc2ecbc9e0f49
daguilarm <damian.aguilarm@gmail.com>
2017-10-09 21:58:54 +0200

    Added list of pests

--- app/Http/Controllers/Dashboard/Agronomic/AgronomicPestsController.php
+++ app/Http/Controllers/Dashboard/Agronomic/AgronomicPestsController.php
@@ -6,7 +6,9 @@ use App\DataTables\AgronomicPests\DataTable;
 use App\Http\Controllers\AgronomicController;
 use App\Http\Requests\AgronomicPestsRequest as AgronomicRequest;
 use App\Repositories\AgronomicPests\AgronomicPestsRepository;
+use App\Repositories\Pests\PestsRepository;
 use App\Repositories\Plots\PlotsRepository;
+use Credentials;
 
 class AgronomicPestsController extends AgronomicController
 {
@@ -14,10 +16,12 @@ class AgronomicPestsController extends AgronomicController
      * @var protected
      */
     protected $section = 'agronomic_pests';
+    protected $pest;
 
-    public function __construct(AgronomicPestsRepository $controller, DataTable $table, PlotsRepository $plot)
+    public function __construct(AgronomicPestsRepository $controller, DataTable $table, PestsRepository $pest, PlotsRepository $plot)
     {
         $this->controller   = $controller;
+        $this->pest         = $pest;
         $this->plot         = $plot;
         $this->table        = $table;
         //Sharing in the view
@@ -25,6 +29,21 @@ class AgronomicPestsController extends AgronomicController
             'section'   => $this->section,
             'role'      => $this->role
         ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //Get the users and the clients
+        list($clients, $users) = $this->controller->getClientUser();
+        $plots = Credentials::isOnlyRole('user') ? $this->plot->listsByRole() : null;
+        $pests = $this->pest->listByCropAndRole();
+            //Get the value
+            return view(dashboard_path($this->section . '.create'), compact('clients', 'pests', 'plots', 'users'));
     }
 
     /**
@@ -36,6 +55,28 @@ class AgronomicPestsController extends AgronomicController
     public function store(AgronomicRequest $request)
     {
         return parent::agronomicStore();
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //Set default data
+        $data = $this->controller->find($id);
+        //Check if the user own the database record
+        //and if the role is authorizate
+        if(!Credentials::authorize($data)) {
+            return Credentials::accessError();
+        }
+        //List of plots
+        $plots = $this->plot->listsByUser($data->user_id) ?? null;
+        $pests = $this->pest->listByCropAndRole();
+            //Return the values
+            return view(dashboard_path($this->section . '.edit'), compact('data', 'pests', 'plots'));
     }
 
     /**
