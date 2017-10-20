77b6e95a4f2f08593ac92c5fb6748508f9f47eba
daguilarm <damian.aguilarm@gmail.com>
2017-10-20 09:46:59 +0200

    New fields

--- resources/lang/es/sections/agronomic_biocides.php
+++ resources/lang/es/sections/agronomic_biocides.php
@@ -7,6 +7,7 @@ return [
     | Sections: agronomic_biocides
     |--------------------------------------------------------------------------
     */
+    'formula'           => 'Formulación',
     'info'              => 'Información sobre fitosanitarios',
     'secure'            => 'Plazo seguridad',
     'title'             => 'Fitosanitario',
