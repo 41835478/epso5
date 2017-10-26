bd1e53d99c62ded4f90d71a00dd2f727790263a3
daguilarm <damian.aguilarm@gmail.com>
2017-10-26 19:46:22 +0200

    Added quantity

--- resources/views/dashboard/agronomic_culturals/forms/sections/quantity.blade.php
+++ resources/views/dashboard/agronomic_culturals/forms/sections/quantity.blade.php
@@ -1,2 +1,12 @@
-{{-- Quantity --}}
-Quantity
+<div class="row">
+    {{-- Field: Quantity  --}}
+    {!! Form::agronomicQuantity($data ?? null) !!}
+
+    {{-- Field: Quantity units --}}
+    <div class="form-group col-md-2">
+        <label class="control-label" for="agronomic_quantity_unit">{{ trans('units.title:mix') }}</label>
+        <select name="agronomic_quantity_unit" id="agronomic_quantity_unit" class="form-control" required="required">
+            <option value="1">Kg</option>
+        </select>
+    </div>
+</div>
